a2e01712aafd0b5f75fd2389db1737b0936281ea
Yuting Wang <yw4554@nyu.edu>
2024-03-24 11:31:30 -0500

    frontend setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header>header</Header>
+      <Header>
+        <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+        </div>
+      </Header>
       <Content
         style={{
           padding: "50px",
@@ -13,7 +27,11 @@ function App() {
           overflowY: "auto",
         }}
       >
-        content
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
       </Content>
     </Layout>
   );
